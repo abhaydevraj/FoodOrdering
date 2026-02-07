c6e21693adf64bdb42933ad2b396c798f4935931
abhaydevraj <101507228+abhaydevraj@users.noreply.github.com>
2026-02-07 14:42:34 +0530

    create product screen done

---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,137 @@
+import Button from "@/src/components/Button";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, Text, TextInput, View } from "react-native";
+
+const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library.
+    // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
+    // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
+    // so the app users aren't surprised by a system dialog after picking a video.
+    // See "Invoke permissions for videos" sub section for more details.
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert(
+        "Permission required",
+        "Permission to access the media library is required.",
+      );
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const validateInput = () => {
+    setError("");
+    if (!name.trim()) {
+      setError("Please enter a product name.");
+      return false;
+    }
+    if (!price.trim() || isNaN(Number(price)) || Number(price) <= 0) {
+      setError("Price is not a number.");
+      return false;
+    }
+    return true;
+  };
+
+  const resetFieds = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+    alert(`Creating new product: ${name}, Price: ${price}`);
+    // save in database logic here
+    resetFieds();
+  };
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text style={styles.textButton} onPress={pickImage}>
+        {" "}
+        Select Image
+      </Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        placeholder="Name"
+        style={styles.input}
+        placeholderTextColor="grey"
+        onChangeText={setName}
+      />
+
+      <Text style={styles.label}>Price</Text>
+      <TextInput
+        value={price}
+        placeholder="9.99"
+        style={styles.input}
+        placeholderTextColor="grey"
+        keyboardType="numeric"
+        onChangeText={setPrice}
+      />
+      <Text style={{ color: "red" }}>{error}</Text>
+      <Button text="Create Product" onPress={onCreate} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 10,
+    borderColor: "#ccc",
+  },
+  label: {
+    color: "grey",
+    fontSize: 16,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+});
+
+export default CreateProductScreen;
